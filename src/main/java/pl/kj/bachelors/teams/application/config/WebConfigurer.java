53965aaf7e29862403a0a7bdcd1391541bc779cd
KJ99 <kj.199961@gmail.com>
2021-12-22 23:40:46 +0100

    enabled cors

---
+++ src/main/java/pl/kj/bachelors/teams/application/config/WebConfigurer.java
@@ -0,0 +1,16 @@
+package pl.kj.bachelors.teams.application.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
+    }
+}
