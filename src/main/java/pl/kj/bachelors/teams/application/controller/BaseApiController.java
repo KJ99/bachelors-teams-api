5d68476322743ad467a27c35a532d581186fc1db
KJ99 <kj.199961@gmail.com>
2021-12-23 02:37:27 +0100

    fix cors

--- src/main/java/pl/kj/bachelors/teams/application/controller/BaseApiController.java
+++ src/main/java/pl/kj/bachelors/teams/application/controller/BaseApiController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import pl.kj.bachelors.teams.application.dto.request.PagingQuery;
@@ -35,6 +36,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+@CrossOrigin
 abstract class BaseApiController {
     @Autowired
     protected ModelMapper mapper;
